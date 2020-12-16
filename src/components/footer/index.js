72ddac460ae60fe0f0614fbd6921ef2d52d69aa0
Tomas Pelak <s3w3nofficial@gmail.com>
2020-12-16 06:02:16 +0100

    first commit

---
+++ src/components/footer/index.js
@@ -0,0 +1,62 @@
+import { h } from "preact";
+import { Link } from "preact-router/match";
+
+const Header = () => (
+  <footer class="footer">
+    <div class="footer-container">
+      <div class="footer-container-shin">
+        <div class="footer-container-shin-content">
+          <h2>
+            Accord is the fast, simple and secure way to communicate with
+            family, friends, teams, colleagues, organisations and the wider
+            world.
+          </h2>
+          <button>Visit github</button>
+          <p>For Web, Android, iOS, macOS, Windows & Linux</p>
+        </div>
+      </div>
+      <div class="footer-container-links">
+        <ul>
+          <li>Products</li>
+          <li>Accord</li>
+          <li>Accord Services</li>
+          <li>Store</li>
+        </ul>
+        <ul>
+          <li>Products</li>
+          <li>Accord</li>
+          <li>Accord Services</li>
+          <li>Store</li>
+        </ul>
+        <ul>
+          <li>Products</li>
+          <li>Accord</li>
+          <li>Accord Services</li>
+          <li>Store</li>
+        </ul>
+        <ul>
+          <li>Products</li>
+          <li>Accord</li>
+          <li>Accord Services</li>
+          <li>Store</li>
+        </ul>
+        <ul>
+          <li>Products</li>
+          <li>Accord</li>
+          <li>Accord Services</li>
+          <li>Store</li>
+        </ul>
+      </div>
+      <div class="footer-container-copyright">
+        <p>Copyright &copy 2020 Accord</p>
+        <div class="footer-container-socials">
+          <img src="https://element.io/images/twitter.svg" alt="" />
+          <img src="https://element.io/images/twitter.svg" alt="" />
+          <img src="https://element.io/images/twitter.svg" alt="" />
+        </div>
+      </div>
+    </div>
+  </footer>
+);
+
+export default Header;
